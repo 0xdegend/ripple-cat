a549dffb6161d52e066da9afaabdf53716fcb40d
Olagboye Seyi <akingbolaola@gmail.com>
2025-01-16 13:42:36 +0100

    Implementing Design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroPage from "./components/HeroPage/HeroPage";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <HeroPage/>
     </>
   );
 }
---
+++ src/components/HeroPage/HeroPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import heropageBlend from "../../assets/images/heropage-blend.svg";
+import heropageCatBlend from "../../assets/images/hero-page-cat.svg";
+import "./HeroPage.css";
+const HeroPage = () => {
+  return (
+    <div className="hero-page-container">
+      <div className="hero-page-details">
+        <div className="hero-page-text">
+          <div className="gradient-text">PLAY. EARN. REPEAT</div>
+          <p>
+            Join the Ultimate Tap-to-Earn Game on Telegram!
+            <br />
+            Turn Every Tap into Rewards—Earn Crypto, Points, and Exclusive
+            Perks!
+          </p>
+        </div>
+        <div className="hero-page-illustration">
+          <div className="hero-page-images">
+            <img
+              src={heropageBlend}
+              alt="Hero Page Blend"
+              className="first-blend"
+            />
+            <img
+              src={heropageCatBlend}
+              alt="Hero Page Cat Blend"
+              className="second-blend"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroPage;
