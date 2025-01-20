b3c164929293aefabb2f32e601e64d00d5d286ae
0xdegend <akingbolaola@gmail.com>
2025-01-20 14:38:30 +0100

    Started implementing Utilities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About/About";
 import HeroPage from "./components/HeroPage/HeroPage";
 import HowItWorks from "./components/HowItWorks/HowItWorks";
 import NavBar from "./components/NavBar/NavBar";
+import Utilities from "./components/Utilities/Utilities";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeroPage />
       <About />
       <HowItWorks />
+      <Utilities/>
     </>
   );
 }
---
+++ src/components/Utilities/Utilities.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Utilities.css";
+const Utilities = () => {
+  return (
+    <div className="utilities-container">
+      <div className="utilities-details"></div>
+    </div>
+  );
+};
+
+export default Utilities;
